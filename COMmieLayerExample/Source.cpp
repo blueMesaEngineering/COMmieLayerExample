037d821e33a8847f0fe3b3c203d3203342e515de
bluemesaengineering <nathan@bluemesaengineering.com>
2023-03-04 00:15:57 +0300

    Set path of link object.

--- COMmieLayerExample/Source.cpp
+++ COMmieLayerExample/Source.cpp
@@ -19,7 +19,26 @@ int main()
 	);
 
 	pDesktop->GetWallpaper(wszWallpaper, MAX_PATH, 0);
+	pDesktop->Release();
 	std::wcout << wszWallpaper;
+
+	IShellLink* pLink = nullptr;
+	CoCreateInstance(
+		CLSID_ShellLink,
+		nullptr,
+		CLSCTX_INPROC_SERVER,
+		__uuidof(IShellLink),
+		reinterpret_cast<void**>(&pLink)
+	);
+
+	pLink->SetPath(wszWallpaper);
+
+	IPersistFile* pPersist = nullptr;
+	pLink->QueryInterface(__uuidof(IPersistFile), reinterpret_cast<void**>(&pPersist));
+
+	pPersist->Save(L"C:\\wallpaper.lnk", FALSE);
+	pPersist->Release();
+	pLink->Release();
 
 	CoUninitialize();
 
